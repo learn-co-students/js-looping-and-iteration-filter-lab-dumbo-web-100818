8638e1107fbcc8526a147bd42cac57d5dc2789a9
ws903 <ws903@nyu.edu>
2018-11-18 16:06:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file
+function findMatching(arr, name) {
+	return arr.filter(function(arr_name) { return arr_name.toLowerCase() === name.toLowerCase() })
+}
+
+function fuzzyMatch(arr, str) {
+	return arr.filter(function(arr_name) { return arr_name.startsWith(str) })
+}
+
+function matchName(arr, name) {
+	return arr.filter(function(obj) {return obj.name.toLowerCase() === name.toLowerCase()})
+}
