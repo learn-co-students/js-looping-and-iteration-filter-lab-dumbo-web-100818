f9aac0d095caddee863e1c6262125759961b4911
said shah <saidmysr20@aol.com>
2018-11-18 21:10:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+function findMatching(givenArray, givenValue) {
+let newArr = givenArray.filter(function (currentValue){return currentValue.toLowerCase() === givenValue.toLowerCase(); });
+return newArr
+}
+function fuzzyMatch(givenArray, givenValue) {
+  let totalChars = givenValue.length
+  let newArray = givenArray.filter(function (isfound){return isfound.slice(0,totalChars)===givenValue; })
+  return newArray
+}
+function matchName(givenData, givenValue){
+  return givenData.filter(function functionName(firstName) {
+      return firstName.name.toLowerCase() === givenValue.toLowerCase()
+
+  })
+}
