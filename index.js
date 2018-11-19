b8428a2300e2baf4a2e7cded982ec92ddc677c28
github username <github email address>
2018-11-19 14:59:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file
+function findMatching(array, name) {
+  const newArr = []
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].toLowerCase() == name.toLowerCase()) {
+      newArr.push(array[i])
+    }
+  }
+
+  return newArr
+}
+
+function fuzzyMatch(array, beginningLetter){
+  const newArr = [];
+  const wordSize = beginningLetter.length
+  for(const driver of array){
+    if(driver.slice(0, wordSize) == beginningLetter){
+      newArr.push(driver)
+    }
+  }
+
+  return newArr
+}
+
+// function matchName(array, value) {
+//   const newArr = [];
+//   for (const user in array){
+//
+//     if (user.name === value){
+//       newArr.push(user)
+//     }
+//   }
+//
+//   return newArr
+// }
+
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
