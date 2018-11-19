39b126924d86fd5261798196cc274a7525b3e06f
Randy Herasme <flatironschool@Flatirons-MacBook-Air.local>
2018-11-19 11:52:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+function findMatching(array, name){
+  return array.filter(function(driverName){
+    upName = driverName.toUpperCase()
+    return upName === name.toUpperCase()
+  })
+}
+
+function fuzzyMatch(array, letter){
+  return array.filter(function(driverName){
+    return driverName[0] === letter[0]
+  })
+}
+
+function matchName(array_of_people, name){
+  return array_of_people.filter(function(driver){
+    return driver.name == name
+  })
+}
